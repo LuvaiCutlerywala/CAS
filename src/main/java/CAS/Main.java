15893985fc128b0cd8523b077fe68514cec84268
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2024-01-05 23:30:41 +0000

    Completed PolynomialParser class.

--- src/main/java/CAS/Main.java
+++ src/main/java/CAS/Main.java
@@ -1,14 +1,21 @@
 package CAS;
 
-import CAS.parser.VectorParser;
+import CAS.parser.PolynomialParser;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args){
-        VectorParser parser = new VectorParser("  (0.2024678293475624375,   2, 4, 127389838723847)    ");
-        double[] vector = parser.getRepresentation();
-        for(double component: vector){
-            System.out.println(component);
+        PolynomialParser parser = new PolynomialParser();
+        parser.parse("12.5x^4 + 3.141592654x^3 + 15x^2 + 9837459x + 32");
+        HashMap<Integer, Double> map = parser.getRepresentation();
+
+        Set<Integer> keys = map.keySet();
+        for (Integer key: keys){
+            System.out.print("Degree = " + key);
+            System.out.println("    Coefficient = " + map.get(key));
         }
     }
 
--- src/main/java/CAS/parser/PolynomialParser.java
+++ src/main/java/CAS/parser/PolynomialParser.java
@@ -2,14 +2,53 @@ package CAS.parser;
 
 import CAS.algebraicObjects.Polynomial;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Pattern;
 
+/**
+ * Extracts the
+ */
 public class PolynomialParser implements Parser<Polynomial> {
 
-    private HashMap<Integer, Double> coefficients;
+    private final HashMap<Integer, Double> coefficients = new HashMap<>();
+
+    public PolynomialParser(){}
+
+    public PolynomialParser(String input){
+        parse(input);
+    }
 
     @Override
     public void parse(String input) {
+        String[] terms = input.strip().split("\\+"); //Terms are split using the addition symbol.
+        for(int i = 0; i < terms.length; ++i){
+            terms[i] = terms[i].strip(); //Strip all terms of leading and trailing whitespace.
+        }
+
+        ArrayList<String> listOfTerms = new ArrayList<>(List.of(terms));
+
+        for(int i = 0; i < listOfTerms.size(); ++i){
+            if(!listOfTerms.get(i).contains("x")){
+                coefficients.put(0, Double.parseDouble(listOfTerms.get(i))); //Filter through for the constant term, and remove it once it has been added to the map.
+                listOfTerms.remove(i);
+                i--;
+            } else if(!listOfTerms.get(i).contains("^")){
+                coefficients.put(1, Double.parseDouble(listOfTerms.get(i).replaceAll("x", ""))); //The same, but for the linear term.
+                listOfTerms.remove(i);
+                i--;
+            }
+        }
+
+        for(String term: listOfTerms){
+            String[] parts = term.split("x");
+            if(parts[0].isBlank()){
+                coefficients.put(Integer.parseInt(parts[1].replaceAll("\\^", "")), 1.0);
+            } else {
+                coefficients.put(Integer.parseInt(parts[1].replaceAll("\\^", "")), Double.parseDouble(parts[0]));
+            }
+        }
 
     }
 
